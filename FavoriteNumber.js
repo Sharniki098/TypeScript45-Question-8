93369bd25d17054ef0eb088e1bf9970fffca331e
Shumaila Noreen <161513956+Sharniki098@users.noreply.github.com>
2024-05-20 01:21:19 -0700

    Add files via upload

---
+++ FavoriteNumber.js
@@ -0,0 +1,3 @@
+var favoriteNumber = 8;
+var message = " here is your favorite number " + favoriteNumber;
+console.log(message);
---
+++ FavoriteNumber.ts
@@ -0,0 +1,5 @@
+const favoriteNumber :number= 8;
+
+const message :String =" here is your favorite number " +  favoriteNumber;
+
+console.log(message)
